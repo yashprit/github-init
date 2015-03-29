234adfbf806b98433bca04f61ae0aba59452494e
Yashprit Singh <yashprits@gmail.com>
2015-03-29 21:34:59 +0530

    configured various plugins

--- index.js
+++ index.js
@@ -11,128 +11,36 @@ var
   isGit = require("is-git"),
   fs = require("fs"),
   Promise = require("bluebird"),
-  request = require("request");
+  request = require("request"),
+  exec = require("child_process").exec;
+
+var gitLocalCreate = require('git-local-create');
+var gitRemoteCreate = require('git-remote-create');
+var gitRemoteAdd = require('git-remote-add');
+var gitRepoUrl = require('git-repo-url');
+var gitFirstCommit = require('git-first-commit');
 
 
-var API_ENDPOINT = "https://api.github.com/repos/yashprit/is-git1"
+var githubInit = function(config) {
 
-var doGet = function(param, callback) {
-  request((API_ENDPOINT + param), function(err, data) {
-    if (err) {
-      callback(err);
-      return
-    }
-
-    if (data.message === "Not Found") {
-      callback(new Error("Error couldn\t find username"))
-      return;
-    }
-
-    if (data.name) {
-      callback(null, data);
-      return;
+  gitLocalCreate(function(err, status) {
+    if (status) {
+      gitRemoteCreate(config.token, config.reponame, function(err, status) {
+        if (status) {
+          gitRemoteAdd(config.username, config.reponame, function(err, status) {
+            if (status) {
+              var message = config.message || "intial commit"
+              gitFirstCommit(config.message, function(err, status) {
+                var logMessage = "\n\n" +
+                  "Git repo is ".blue + gitRepoUrl.https(config.username, config.reponame).green + "\n"
+                console.log(logMessage);
+              });
+            }
+          })
+        }
+      })
     }
   });
-}
-
-/**
- * Promise wrapper around isGit
- *
- * @api @private
- *
- * @return {Boolean} is current directory is git directory or not
- */
-var checkGit = function(fn) {
-  return new Promise(function(resolve, reject) {
-    isGit(function(err, exist) {
-      if (err) reject(err);
-      resolve(exists);
-    });
-  });
-}
-
-/**
- * run git init locally
- *
- * @api @private
- *
- * @throws {Error} If git init command fails
- *
- * @return {Promise} status of command
- *
- */
-var doGitInit = function() {
-
-}
-
-/**
- * Check for remote repo present on not
- * @return {Boolean} file present or not
- */
-var checkForRemote = function() {
-
-}
-
-/**
- * checks for repository exists, if not create new NODEJS repository
- *
- * @api @private
- *
- * @throws {Error} If remote send anything other than 200 OK
- *
- * @return {Promise} status of command
- */
-var createRemoteRepo = function() {
-
-}
-
-/**
- * Link local repository with remote repository
- *
- * @api @private
- *
- * @return {Boolean} flag to tell success or error
- */
-var doRemoteAdd = function() {
-
-}
-
-/**
- * Check if local and remote are linked to each other or not
- *
- * @api @private
- *
- * @return {Boolean} status of link
- */
-var checkForremote = function() {
-
-}
-
-/**
- * Template function call all internal api as chainable promises
- *
- * @api @public
- *
- * @throws {Error} If any internal methods throws it
- */
-var githubInit = function(username, token) {
-  var module = process.cwd().split("/").pop(0);
-
-  checkGit()
-    .then(function(exists) {
-      if (exists) {
-        createRemoteRepo({
-          username: username,
-          token: token,
-          repo: repo
-        }, function(err, data) {
-          console.log(data);
-        })
-      }
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
 }
 
 module.exports = githubInit;
